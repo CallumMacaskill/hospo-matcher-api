9173778ad4d6ed7d1947d8dc0d753aa3330ce78f
Callum Macaskill <cal.macaskill@gmail.com>
2024-06-19 23:46:36 +1200

    WIP: refactor fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,33 +1,24 @@
-from hospo_matcher.app import app
-from hospo_matcher.utils.data_models import settings
-from hospo_matcher.utils.database import Driver, driver
-
-from pytest import fixture
 from httpx import AsyncClient
+from pytest import fixture
 
-from tests.data import initialise_db
+from hospo_matcher.app import app
+from hospo_matcher.utils.database import driver
+from tests.data import test_db_client
 
 
 @fixture(scope="session")
-async def async_client():
+async def async_client(test_db_client):
     def _override_get_db_client():
-        return test_driver.get_db_client("test_db_test")
-    
-    # Initialise test database
-    await initialise_db(settings.MONGODB_TEST_NAME)
+        return test_db_client
 
     # Override dependency to use the custom test driver database client
-    test_driver = Driver()
     app.dependency_overrides[driver.get_db_client] = _override_get_db_client
 
     # Testing async endpoints that await MongoDB operations requires an async client.
     async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as async_client:
         yield async_client
 
-    # Tear down db
-    test_driver.mongo_client.drop_database(settings.MONGODB_TEST_NAME)
-
 
 @fixture(scope="session")
 def anyio_backend():
-    return 'asyncio'
+    return "asyncio"
--- tests/data.py
+++ tests/data.py
@@ -1,14 +1,70 @@
-from hospo_matcher.utils.data_models import Session, Venue
+import asyncio
+from argparse import ArgumentParser
+from random import choice, randint
+
+import pytest
+from motor.motor_asyncio import AsyncIOMotorDatabase
+
+from hospo_matcher.utils.data_models import RegionCodes, Session, Venue, settings
 from hospo_matcher.utils.database import Driver
-from hospo_matcher.utils.data_models import RegionCodes
 from hospo_matcher.utils.logger import log
 
-from random import randint, choice
-import asyncio
 
-from argparse import ArgumentParser
+@pytest.fixture(scope="session", autouse=True)
+async def synthetic_venues(test_db_client: AsyncIOMotorDatabase) -> list[str]:
+    # Generate objects
+    region_codes = list(RegionCodes)
+    venues = []
+    for i in range(50):
+        venue = Venue(
+            id=str(i),
+            name=f"Venue #{i}",
+            region=choice(region_codes),
+            hour_open=randint(6, 10),
+            hour_closed=randint(17, 24),
+        )
+        venues.append(venue.model_dump())
+    log.info(f"Created {len(venues)} synthetic venues")
 
-async def initialise_db(db_name: str):
+    # Insert into collection
+    result = await test_db_client.venues.insert_many(venues)
+    log.info(f"Inserted {len(result.inserted_ids)} venues")
+    return [str(id) for id in result.inserted_ids]
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def synthetic_sessions(
+    test_db_client: AsyncIOMotorDatabase, synthetic_venues: list[str]
+) -> list[str]:
+    # Generate venues
+    region_codes = list(RegionCodes)
+    sessions = []
+    for i in range(5):
+        session = Session(
+            code=f"session_{i}",
+            location=choice(region_codes),
+            user_votes={
+                f"User {i}{j}": [
+                    str(choice(synthetic_venues)) for k in range(randint(5, 10))
+                ]
+                for j in range(randint(2, 10))
+            },
+        )
+        sessions.append(session.model_dump())
+    log.info(f"Created {len(sessions)} synthetic venues")
+
+    # Create unique code constraint and insert into collection
+    await test_db_client.sessions.create_index("code", unique=True)
+    result = await test_db_client.sessions.insert_many(sessions)
+    log.info(f"Inserted {len(result.inserted_ids)} sessions")
+    return [str(id) for id in result.inserted_ids]
+
+
+# TODO: Make async autouse fixtures work. Look into libs and async auto config.
+
+
+@pytest.fixture(scope="session")
+async def test_db_client():
     """
     Create a new MongoDB database with collections and pre-filled data.
 
@@ -16,59 +72,25 @@ async def initialise_db(db_name: str):
     """
     # Construct test db and collections and load data
     driver = Driver()
-    db = driver.get_db_client(db_name)
-    log.info(f"Created db {db_name}")
+    db = driver.get_db_client(settings.MONGODB_TEST_NAME)
+    log.info(f"Providing db client for {settings.MONGODB_TEST_NAME}")
 
-    # Initialise venues data
-    venues = generate_synthetic_venues()
-    venues_result = await db.venues.insert_many(venues)
-    log.info(f"Inserted {len(venues_result.inserted_ids)} venues")
+    # Provide database client for tests
+    yield db
 
-    # Initialise sessions collection and data
-    await db.sessions.create_index("code", unique=True)
-    sessions = generate_synthetic_sessions(venues_result.inserted_ids)
-    sessions_result = await db.sessions.insert_many(sessions)
-    log.info(f"Inserted {len(sessions_result.inserted_ids)} sessions")
+    # Tidy up database after tests have run
+    driver.mongo_client.drop_database(settings.MONGODB_TEST_NAME)
+    log.info(f"Dropped db {settings.MONGODB_TEST_NAME}")
 
-    return db
 
-def generate_synthetic_venues(n_venues: int = 50) -> list[dict]:
-    # Generate venues
-    region_codes = list(RegionCodes)
-    venues = []
-    for i in range(n_venues):
-        venue = Venue(
-            id=str(i),
-            name=f"Venue #{i}",
-            region=choice(region_codes),
-            hour_open=randint(6, 10),
-            hour_closed=randint(17, 24)
-        )
-        venues.append(venue.model_dump())
-    return venues    
-
-def generate_synthetic_sessions(venue_ids: list[str], n_sessions: int = 5) -> list[dict]:
-    # Generate venues
-    region_codes = list(RegionCodes)
-    sessions = []
-    for i in range(n_sessions):
-        session = Session(
-            code=f"session_{i}",
-            location=choice(region_codes),
-            user_votes={f"User {i}{j}": [str(choice(venue_ids)) for k in range(randint(5, 10))] for j in range(randint(2, 10))}
-        )
-        sessions.append(session.model_dump())
-    return sessions
-
-if __name__=="__main__":
+if __name__ == "__main__":
     # Get inputs
     arg_parser = ArgumentParser()
     arg_parser.add_argument(
-        "--db-name",
-        help="Name of the MongoDB database to be created."
+        "--db-name", help="Name of the MongoDB database to be created."
     )
     args = arg_parser.parse_args()
     db_name = args.db_name
 
     # Create new db
-    asyncio.run(initialise_db(db_name))
+    # asyncio.run(initialise_db(db_name))
