76737e14600d85c597ab48fa30a4afbdf7fe588f
Callum Macaskill <cal.macaskill@gmail.com>
2024-06-29 18:19:21 +1200

    Value comparison in venue read test

--- tests/test_venues.py
+++ tests/test_venues.py
@@ -1,7 +1,6 @@
-from hospo_matcher.utils.logger import log
-
-
-async def test_read_venues(async_client):
+async def test_read_valid_venues(async_client, synthetic_venues):
     response = await async_client.get("/venues/")
+    venues = response.json()
     assert response.status_code == 200
     assert len(response.json()) == 10
+    assert all(venue in synthetic_venues for venue in venues)
