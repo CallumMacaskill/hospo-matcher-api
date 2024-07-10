10e0361e5d1e5dea1208832053096b04d9fe9517
Callum Macaskill <cal.macaskill@gmail.com>
2024-07-10 22:31:02 +1200

    Create valid and invalid session tests

--- tests/test_sessions.py
+++ tests/test_sessions.py
@@ -1,11 +1,9 @@
 import random
 
-class TestRead:
-    async def test_read_root(self, async_client):
-        response = await async_client.get("/")
-        assert response.status_code == 200
-        assert response.json() == {"message": "API is running."}
+from httpx import AsyncClient
 
+
+class TestRead:
     async def test_read_valid_session(self, async_client, synthetic_sessions):
         """
         Read a valid session.
@@ -16,9 +14,27 @@ class TestRead:
         response_session = response.json()
         assert response_session == sample_session
 
-    async def test_read_bad_session(self, async_client):
+    async def test_read_invalid_session(self, async_client):
         """
         Read a session that does not exist.
         """
         response = await async_client.get("/sessions/123")
         assert response.status_code == 404
+
+
+class TestCreate:
+    async def test_create_valid_session(self, async_client: AsyncClient):
+        body = {
+            "code": "valid_code",
+            "location": "NZ-AUK",
+        }
+        response = await async_client.post("/sessions/", json=body)
+        assert response.status_code == 200
+
+    async def test_create_invalid_duplicate_code(
+        self, async_client: AsyncClient, synthetic_sessions
+    ):
+        sample_venue = synthetic_sessions[0].copy()
+        body = {"code": sample_venue["code"], "location": sample_venue["location"]}
+        response = await async_client.post("/sessions/", json=body)
+        assert response.status_code == 400
